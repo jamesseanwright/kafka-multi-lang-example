2eef4e45a395869d367fee339c7208263308ab07
James Wright <hi@james.engineering>
2023-12-03 16:32:29 +0000

    Adds Spring Boot boilerplate

---
+++ java/src/main/java/engineering/james/kafkaexample/KafkaexampleApplication.java
@@ -0,0 +1,13 @@
+package engineering.james.kafkaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaexampleApplication.class, args);
+	}
+
+}
