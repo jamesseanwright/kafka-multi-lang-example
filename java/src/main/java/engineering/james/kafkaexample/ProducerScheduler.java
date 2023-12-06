60aad7c1670a0d076100295b747b0fd1f77f0d55
James Wright <hi@james.engineering>
2023-12-06 20:23:11 +0000

    Handles Producer.send() exceptions

--- java/src/main/java/engineering/james/kafkaexample/ProducerScheduler.java
+++ java/src/main/java/engineering/james/kafkaexample/ProducerScheduler.java
@@ -1,10 +1,15 @@
 package engineering.james.kafkaexample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import engineering.james.kafkaexample.events.Event;
+import engineering.james.kafkaexample.events.UnrecognisedTopicException;
 
 import static engineering.james.kafkaexample.events.EventFactory.generateEventForTopic;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +22,19 @@ public class ProducerScheduler {
     @Value("${application.producer.kafka-topic}")
     private String kafkaTopic;
 
+    private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
     @Scheduled(fixedRate = 1000)
-    public void produce() throws Exception {
-        // TODO: handle JsonProcessingException & factory exception
-        // TODO: await and log success + failure
-        Event event = generateEventForTopic(this.kafkaTopic);
-        this.producer.send(event);
+    public void produce() {
+        this.logger.info("Sending event to {} topic", kafkaTopic);
+
+        try {
+            Event event = generateEventForTopic(this.kafkaTopic);
+            this.producer.send(event);
+        } catch (UnrecognisedTopicException e) {
+            this.logger.error("Unable to generate event for topic", e);
+        } catch (JsonProcessingException e) {
+            this.logger.error("Failed to serialise CloudEvent", e);
+        }
     }
 }
