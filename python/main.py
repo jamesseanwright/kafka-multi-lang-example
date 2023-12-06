66cdc40440feb02cc5a2cda820b48d496e703afe
James Wright <hi@james.engineering>
2023-12-06 15:39:09 +0000

    Correctly maps Python Kafka consumer record to CloudEvent KafkaMessage

--- python/main.py
+++ python/main.py
@@ -2,26 +2,30 @@ import asyncio
 import logging
 import os
 from aiokafka import AIOKafkaConsumer
-from cloudevents import from_json
+from aiokafka.structs import ConsumerRecord
+from cloudevents.kafka import from_binary, KafkaMessage
 
 consumer_group_id = os.environ["APPLICATION_CONSUMER_GROUP_ID"]
 logger = logging.getLogger(consumer_group_id)
 
 logging.basicConfig(level=logging.INFO, force=True)
 
+def to_message(record: ConsumerRecord):
+    return KafkaMessage(dict(record.headers), record.key, record.value)
+
 async def consume():
     consumer = AIOKafkaConsumer(
         os.environ["APPLICATION_CONSUMER_KAFKA_TOPIC"],
         group_id=consumer_group_id,
         bootstrap_servers=["kafka:29092"],
-        value_deserializer=lambda x: from_json(x) # TODO!
     )
 
     await consumer.start()
 
     try:
-        async for message in consumer:
-            logger.info(message.value.decode())
+        async for record in consumer:
+            cloudevent = from_binary(to_message(record))
+            logger.info(cloudevent)
     finally:
         await consumer.stop()
 
